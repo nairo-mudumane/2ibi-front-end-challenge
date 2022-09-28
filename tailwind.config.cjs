0a77603946683ccfb0bb182c0b7d5a6fc2f3cc09
nairo-mudumane <nairomudumane@outlook.com>
2022-09-28 08:49:15 +0200

    google fonts - roboto

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.tsx", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Roboto, sans-serif", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
